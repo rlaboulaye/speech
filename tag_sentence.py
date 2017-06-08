e3e65b74c5a9332a4c9c672d9e5f0cea167e28a5
Roland Laboulaye <rlaboulaye@gmail.com>
2017-06-08 15:37:24 -0600

    final

--- tag_sentence.py
+++ tag_sentence.py
@@ -9,24 +9,9 @@ trans_p, emit_p = get_pos_model(training_data)
 states = list(trans_p.keys())
 start_p = trans_p['.']
 
-correct = 0
-outof = 0
-for sentence in text.split('._.'):
-    if sentence != '' and sentence != '\n':
-        sentence += '._.'
-        obs = []
-        target = []
-        for word_label_pair in sentence.split():
-            word_arr = word_label_pair.split('_')
-            obs.append(word_arr[0])
-            target.append(word_arr[1])
-        assignment = viterbi(obs, states, start_p, trans_p, emit_p)
-        print(obs)
-        print(target)
-        print(assignment)
-        for i in range(len(target)):
-            outof += 1
-            if target[i] == assignment[i]:
-                correct += 1
+sentence = 'I like dogs .'
+obs = sentence.split()
 
-print('Test Accuracy: ', correct / outof)
+assignment = viterbi(obs, states, start_p, trans_p, emit_p)
+print(obs)
+print(assignment)
